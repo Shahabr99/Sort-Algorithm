eb3e2be2b5edf50849b6953f3c0bd401d2ec1e84
Shahabr99 <sean.rohani9@gmail.com>
2023-12-03 21:21:15 -0800

    selectionSort

--- insertion.js
+++ insertion.js
@@ -1,5 +1,19 @@
 function insertionSort(arr) {
-  
+  for(let i = 1; i < arr.length; i++) {
+    let currentEl = arr[i]
+    let j = i - 1;
+    
+    while(j >= 0 && arr[j] > currentEl) {
+      arr[j + 1] = arr[j]
+      console.log(j)
+      console.log(i)
+      j--
+    }
+
+    arr[j + 1] = currentEl;
+  }
+
+  return arr;
 }
 
 module.exports = insertionSort;
--- selection.js
+++ selection.js
@@ -1,3 +1,24 @@
-function selectionSort() {}
+function selectionSort(arr) {
+
+  for(let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    // Find the index of the minimum element in the unsorted part of the array
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+
+    // Swap the found minimum element with the first element
+    if(minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
 
 module.exports = selectionSort;
