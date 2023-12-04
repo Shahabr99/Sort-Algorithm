3380b642062282d0a53a8afadd15d14e85ea6dd9
Shahabr99 <sean.rohani9@gmail.com>
2023-12-03 18:24:48 -0800

    merge completed

--- merge.js
+++ merge.js
@@ -1,4 +1,28 @@
 function merge(arr1, arr2) {
+  const results = []
+  let i = 0
+  let j = 0
+
+  while(i < arr1.length && j < arr2.length) {
+    if(arr1[i] < arr2[j]) {
+      results.push(arr1[i])
+      i++
+    }else{
+      results.push(arr2[j])
+      j++
+    }
+  }
+  
+  while(i < arr1.length) {
+    results.push(arr1[i])
+    i++
+  }
+  while(j < arr2.length) {
+    results.push(arr2[j])
+    j++
+  }
+  
+  return results;
   
 }
 
@@ -10,9 +34,10 @@ function mergeSort(arr) {
   const mid = Math.floor(arr.length / 2)
 
   // creating two arrays
-  const left = arr.slice(0, mid)
-  const right = arr.slice(mid)
-
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  console.log(`left:${left}`)
+  console.log(`right: ${right}`)
   return merge(left, right)
 }
 
